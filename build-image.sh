a050635a4800e76dd446730df08b92e09c0a97b9
Andrii Kudriashov <andrii@kudriashov.net>
2023-11-20 21:01:31 +0100

    allow root

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source submodule/poky/oe-init-build-env
-cp yocto-conf/* conf/.
+touch conf/sanity.conf
+cp ../yocto-conf/* conf/.
 bitbake zephyr-blinky
 ls -alh tmp-newlib/deploy/images/nrf52840dk-nrf52840/
