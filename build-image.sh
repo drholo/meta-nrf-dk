d0691feccac4410f127c2c8c242c1cd342349f1e
Andrii Kudriashov <andrii@kudriashov.net>
2023-11-20 21:33:58 +0100

    build as yocto user

--- build-image.sh
+++ build-image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-source submodule/poky/oe-init-build-env
-touch conf/sanity.conf
-cp ../yocto-conf/* conf/.
+source /workspace/submodule/poky/oe-init-build-env
+# touch conf/sanity.conf
+cp /workspace/yocto-conf/* conf/.
 bitbake zephyr-blinky
-ls -alh tmp-newlib/deploy/images/nrf52840dk-nrf52840/
